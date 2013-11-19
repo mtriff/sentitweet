b11a030b23c09c8cbd28d3942973fb8c01109432
sleye <matt.triff@gmail.com>
2013-11-19 01:26:25 -0600

    Implemented Twitter API, now able to retrieve and display tweets

--- app/common/sentiTweet/sentiTweet.js
+++ app/common/sentiTweet/sentiTweet.js
@@ -2,11 +2,51 @@ var app=angular.module('sentiTweets', []);
 
 app.controller('BrowseCtrl', function($scope, theServer)
 {
-	$scope.tweets=theServer.getTweets();
+	//$scope.auth=theServer.authorize();
+	
+  console.log('Called BrowseCtrl');
+	$scope.tweets=[
+		{user: {screen_name: 'TestUser'}, text: 'Text of Tweet Here'}
+	];
+
+	var socket=io.connect('http://localhost:8081');
+	window.socket=socket;
+	socket.on('newTweet', function(theTweet)
+	{
+		$scope.tweets.unshift(theTweet);
+		$scope.$apply();
+	});
+
+	theServer.getTweets();
 });
 
-app.factory('theServer', function($http, $q) {
+// function BrowseCtrl($scope)
+// {
+// 	console.log('Called BrowseCtrl');
+// 	$scope.tweets=[{user: {screen_name: 'TestUser'}, text: 'Text of Tweet Here'}];
+
+// 	var socket=io.connect('http://localhost:8081');
+// 	window.socket=socket;
+// 	socket.on('newTweet', function(theTweet)
+// 	{
+// 		$scope.tweets.push(theTweet);
+// 		$scope.$apply();
+// 	});
+// }
+
+ app.factory('theServer', function($http, $q) {
 	return {
+		authorize: function()
+		{
+			var deferred=$q.defer();
+			$http.get('/auth/twitter').success(function(data) {
+				console.log(data);
+				deferred.resolve(data);
+			}).error(function()
+			{
+				deferred.reject();
+			});
+			return deferred.promise;		},
 		getTweets: function()
 		{
 			var deferred=$q.defer();
